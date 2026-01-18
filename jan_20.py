560ff6f6e98e00cd3855c6dd0dcdcc27c47efd66
Beltyukova <svetlana.beltyukova@gmail.com>
2026-01-18 14:33:44 -0500

    Create a starter file

---
+++ jan_20.py
@@ -0,0 +1,62 @@
+"""
+Lesson goal:
+- Understand what an expression is
+- Practice writing hard-coded expressions of different types
+- Introduction to primitive data types:
+    Integer
+    Float 
+    String (text)
+    Boolean (True/False)
+- Introduction to built-in functions: 
+    print()
+- Practice passing the value produced by an expression to the print function
+  and printing it to the terminal
+"""
+# Live-coding starts here... For each task:
+    # - use print() function 
+    # - use hard-coded values inside the function
+    # - what you see in parentheses () is an expression
+    # - run code to see results in the terminal
+
+
+# 1. Write an expression that evaluates to an integer.
+# Pass it to the print function & print the result to the terminal.
+
+
+# 2. Write an expression that evaluates to a float.
+# Pass it to the print function & print the result to the terminal.
+
+
+# 3. Write expressions with arithmetic operations
+# (addition, subtraction, multiplication, division).
+# Pass it to the print function & print the result to the terminal.
+
+
+# 4. Write expressions to practice order precedence of arithmetic operations.
+# Pass it to the print function & print the result to the terminal.
+
+
+# 5. Write expressions that produce a letter F like shown.
+# Pass it to the print function & print the result to the terminal.
+# ********
+# ********
+# **
+# **
+# *****
+# **
+# **
+# **
+
+
+# 6. Write an expression, comparing numbers.
+# This expression evaluates to a Boolean value.
+# Pass it to the print function & print the result to the terminal.
+
+
+# 7. Write an expression that evaluates to a string.
+# Pass it to the print function.
+# Print the result to the terminal.
+
+
+# 8. Write an expression that concatenates a string and a numeric value into a string.
+# Pass it to the print function & print the result to the terminal.
