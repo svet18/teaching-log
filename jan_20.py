9241e903dace6b113ad5650491cdb351bd883b80
Beltyukova <svetlana.beltyukova@gmail.com>
2026-01-20 12:59:17 -0500

    Print hardcoded values

--- jan_20.py
+++ jan_20.py
@@ -13,28 +13,32 @@ Lesson goal:
   and printing it to the terminal
 """
 # Live-coding starts here... For each task:
-    # - use print() function 
-    # - use hard-coded values inside the function
-    # - what you see in parentheses () is an expression
-    # - run code to see results in the terminal
+# - use print() function
+# - use hard-coded values inside the function
+# - what you see in parentheses () is an expression
+# - run code to see results in the terminal
 
 
 # 1. Write an expression that evaluates to an integer.
 # Pass it to the print function & print the result to the terminal.
-
+print(13)
 
 # 2. Write an expression that evaluates to a float.
 # Pass it to the print function & print the result to the terminal.
-
+print(2.7)
 
 # 3. Write expressions with arithmetic operations
 # (addition, subtraction, multiplication, division).
 # Pass it to the print function & print the result to the terminal.
-
+print(2 + 2)
+print(10 - 3)
+print(3 * 11)
+print(12/4)
 
 # 4. Write expressions to practice order precedence of arithmetic operations.
 # Pass it to the print function & print the result to the terminal.
-
+print(2 + 2 * 3/2)
+print(((2 + 2) * 3)/2)
 
 # 5. Write expressions that produce a letter F like shown.
 # Pass it to the print function & print the result to the terminal.
@@ -46,17 +50,26 @@ Lesson goal:
 # **
 # **
 # **
-
+print("*" * 8)
+print("*" * 8)
+print("*" * 2)
+print("*" * 2)
+print("*" * 5)
+print("*" * 2)
+print("*" * 2)
+print("*" * 2)
 
 # 6. Write an expression, comparing numbers.
 # This expression evaluates to a Boolean value.
 # Pass it to the print function & print the result to the terminal.
-
+print(2 > 3)
+print(10 < 20)
 
 # 7. Write an expression that evaluates to a string.
 # Pass it to the print function.
 # Print the result to the terminal.
-
+print("Today is Tuesday")
 
 # 8. Write an expression that concatenates a string and a numeric value into a string.
 # Pass it to the print function & print the result to the terminal.
+print("Today is Tuesday, " + "January 20, ", 2026)
