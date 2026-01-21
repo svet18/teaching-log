33ed8fce04e7cc54f8567819518794827d5767c5
Beltyukova <svetlana.beltyukova@gmail.com>
2026-01-21 06:51:29 -0500

    Remove redundancy in comments & make code easier to read

--- jan_20.py
+++ jan_20.py
@@ -11,37 +11,36 @@ Lesson goal:
     print()
 - Practice passing the value produced by an expression to the print function
   and printing it to the terminal
+
+For each task:
+    - use print() function
+    - use hard-coded values inside the function
+    - what you see in parentheses () is an expression
+    - write a required expression, 
+    - pass it to the print function & 
+    - print the result to the terminal.
 """
-# Live-coding starts here... For each task:
-# - use print() function
-# - use hard-coded values inside the function
-# - what you see in parentheses () is an expression
-# - run code to see results in the terminal
 
-
-# 1. Write an expression that evaluates to an integer.
-# Pass it to the print function & print the result to the terminal.
+# 1. Expression that evaluates to an integer.
 print(13)
 
-# 2. Write an expression that evaluates to a float.
-# Pass it to the print function & print the result to the terminal.
+# 2. Expression that evaluates to a float.
 print(2.7)
 
-# 3. Write expressions with arithmetic operations
-# (addition, subtraction, multiplication, division).
-# Pass it to the print function & print the result to the terminal.
+# 3. Expressions with arithmetic operations
 print(2 + 2)
 print(10 - 3)
 print(3 * 11)
 print(12/4)
 
-# 4. Write expressions to practice order precedence of arithmetic operations.
-# Pass it to the print function & print the result to the terminal.
+# 4. Expressions to practice order precedence of arithmetic operations:
+#   - parentheses first
+#   - multiplication/division second
+#   - addition/subrtaction third
 print(2 + 2 * 3/2)
 print(((2 + 2) * 3)/2)
 
-# 5. Write expressions that produce a letter F like shown.
-# Pass it to the print function & print the result to the terminal.
+# 5. Expressions that produce a letter F like shown.
 # ********
 # ********
 # **
@@ -59,17 +58,12 @@ print("*" * 2)
 print("*" * 2)
 print("*" * 2)
 
-# 6. Write an expression, comparing numbers.
-# This expression evaluates to a Boolean value.
-# Pass it to the print function & print the result to the terminal.
+# 6. Boolean expression, comparing numbers.
 print(2 > 3)
 print(10 < 20)
 
-# 7. Write an expression that evaluates to a string.
-# Pass it to the print function.
-# Print the result to the terminal.
+# 7. Expression that evaluates to a string.
 print("Today is Tuesday")
 
-# 8. Write an expression that concatenates a string and a numeric value into a string.
-# Pass it to the print function & print the result to the terminal.
+# 8. Expression that concatenates a string and a numeric value into a string.
 print("Today is Tuesday, " + "January 20, ", 2026)
